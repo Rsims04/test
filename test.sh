a92db41e9911ee0b9f939f876fffc9d2d6f0352d
Rsims04 <rtsims04@gmail.com>
2024-09-10 23:19:27 +0930

    test: added files

--- test.sh
+++ test.sh
@@ -3,16 +3,18 @@ git status
 git log graph
 
 git checkout test
+git status
 echo package.json
 git add package.json
+git status
 git commit -m "hello"
+git status
 # git reset --hard
 git rebase origin main
 git status
-git log --graph
 git push -f
+git status
 
 git checkout main
 git status
-git log --graph
 
