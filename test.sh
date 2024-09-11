99583f770b61f5aee38ef166ab85049ddab54a11
Rsims04 <rtsims04@gmail.com>
2024-09-11 20:08:25 +0930

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,23 +1,17 @@
 # git checkout test
 git status
-git log graph
+git log --graph
 
 git checkout test
 git status
 
 echo "1" >> package.json
 git add package.json
-git status
-
 git commit -m "hello"
-git status
-
 git reset --hard
 
 git checkout main
 git rebase test
-git status
-
 git push -f
 git status
 
